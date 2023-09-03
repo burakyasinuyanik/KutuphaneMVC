5159aa9e48e757833e8e5f953a2880ff1a5a4c84
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2023-09-03 10:34:24 +0300

    Add project files.

---
+++ Kutuphane.Models/YayinEvi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane.Models
+{
+    public class YayinEvi:BaseModel
+    {
+        public string Tel { get; set; }
+        public string Adres { get; set;}
+
+        public virtual ICollection<Kitap> Kitaplar { get; set; } = new List<Kitap>();
+        public virtual ICollection<Yazar> Yazarlar { get; set; } = new List<Yazar>();
+    }
+}
---
+++ Kutuphane.Models/Yazar.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kutuphane.Models
+{
+    [Table("Yazarlar")]
+    public class Yazar:BaseModel
+    {
+        public string? Ozgecmis { get; set; }
+        public virtual ICollection<YayinEvi> YayinEvleri { get; set; } = new List<YayinEvi>();
+        public virtual ICollection<Kitap> Kitaplar { get; set; } = new List<Kitap>();
+
+    }
+}
